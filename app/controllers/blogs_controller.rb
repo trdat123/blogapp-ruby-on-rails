2d6a3ef6a637e3bd0b2f27f088fb19beae35c0e5
Nguyen Dat <nkokviper123@gmail.com>
2021-04-15 00:04:24 +0700

    update ui, add username to devise(by modify controller), associations

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,71 +1,72 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
-  skip_before_action :verify_authenticity_token
+    before_action :set_blog, only: %i[ show edit update destroy ]
+    skip_before_action :verify_authenticity_token
 
-  # GET /blogs or /blogs.json
-  def index
-    @blogs = Blog.all
-    @blog = Blog.new
-  end
-
-  # GET /blogs/1 or /blogs/1.json
-  def show
-  end
-
-  # GET /blogs/new
-  def new
-    @blog = Blog.new
-  end
-
-  # GET /blogs/1/edit
-  def edit
-  end
-
-  # POST /blogs or /blogs.json
-  def create
-    @blog = Blog.new(blog_params)
-
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to index, notice: "Blog was successfully created." }
-        format.json { render :show, status: :created, location: index }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /blogs/1 or /blogs/1.json
-  def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: "Blog was successfully updated." }
-        format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /blogs/1 or /blogs/1.json
-  def destroy
-    @blog.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
+    # GET /blogs or /blogs.json
+    def index
+        @blogs = Blog.all
+        @blog = Blog.new
     end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:blog_text)
+    # GET /blogs/1 or /blogs/1.json
+    def show
     end
+
+    # GET /blogs/new
+    def new
+        @blog = Blog.new
+    end
+
+    # GET /blogs/1/edit
+    def edit
+    end
+
+    # POST /blogs or /blogs.json
+    def create
+        @blog = Blog.new(blog_params)
+        @blog.user = current_user
+
+        respond_to do |format|
+        if @blog.save
+            format.html { redirect_to index, notice: "Blog was successfully created." }
+            format.json { render :show, status: :created, location: index }
+        else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @blog.errors, status: :unprocessable_entity }
+        end
+        end
+    end
+
+    # PATCH/PUT /blogs/1 or /blogs/1.json
+    def update
+        respond_to do |format|
+        if @blog.update(blog_params)
+            format.html { redirect_to @blog, notice: "Blog was successfully updated." }
+            format.json { render :show, status: :ok, location: @blog }
+        else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @blog.errors, status: :unprocessable_entity }
+        end
+        end
+    end
+
+    # DELETE /blogs/1 or /blogs/1.json
+    def destroy
+        @blog.destroy
+        respond_to do |format|
+        format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
+        format.json { head :no_content }
+        end
+    end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_blog
+        @blog = Blog.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def blog_params
+        params.require(:blog).permit(:blog_text)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_144156) do
+ActiveRecord::Schema.define(version: 2021_04_14_160158) do
 
   create_table "blogs", force: :cascade do |t|
     t.text "blog_text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_144156) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
